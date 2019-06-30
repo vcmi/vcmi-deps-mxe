0567eafa1fa101db57bbdb5a82f5974ac2f6bf5b
Dydzio <dydzio0614@users.noreply.github.com>
2019-06-30 22:49:32 +0200

    Update old MXE repo mirroring script

--- mirror-mxe.sh
+++ mirror-mxe.sh
@@ -4,10 +4,10 @@ export MIRROR_DATE=`date +"%Y-%m-%d"`
 
 sudo apt-get clean
 
-echo "deb http://pkg.mxe.cc/repos/apt/debian wheezy main" \
+echo "deb http://pkg.mxe.cc/repos/apt trusty main" \
     | sudo tee /etc/apt/sources.list.d/mxeapt.list
 
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D43A795B73B16ABE9643FE1AFD8FFF16DB45C6AB
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 86B72ED9
 
 sudo apt-get update -qq
 sudo apt-get install -q --yes --download-only \
@@ -21,9 +21,10 @@ mxe-$MXE_TARGET-sdl2-mixer \
 mxe-$MXE_TARGET-sdl2-ttf \
 mxe-$MXE_TARGET-ffmpeg \
 mxe-$MXE_TARGET-qt \
-mxe-$MXE_TARGET-qtbase
+mxe-$MXE_TARGET-qtbase \
+mxe-i686-w64-mingw32.static-luajit
 
-sudo apt-key del D43A795B73B16ABE9643FE1AFD8FFF16DB45C6AB
+sudo apt-key del 86B72ED9
 sudo rm /etc/apt/sources.list.d/mxeapt.list
 
 cd /var/cache/apt/archives/
